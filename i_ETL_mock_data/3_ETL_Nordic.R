cc2a73e30344d884aaafc3f45d821147ad0cb9f9
Claudia Bartolini <clabar@arsanita.toscana.it>
2021-10-04 13:55:41 +0200

    added ETL and parameters Nordic

---
+++ i_ETL_mock_data/3_ETL_Nordic.R
@@ -0,0 +1,71 @@
+#################################################################################################
+## ETL ConcePTION ARS ####
+
+# Version 2.2
+# 04-10-2021
+# Author: CB 
+
+#################################################################################################
+
+rm(list=ls(all.names=TRUE))
+
+# set the directory where the file is saved as the working directory
+thisdir<-dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(thisdir)
+
+#load parameters
+source(paste0(thisdir,"/parameters/parameters_Nordic.R"))
+setwd(thisdir)
+
+
+# 6.	Use ANAFULL to populate PERSONS, OBSERVATION_PERIODS --------
+ANAFULL <- fread(paste0(dirinput,"/ANAFULL.csv"))
+setkeyv(ANAFULL,"ID")
+
+## PERSONS:
+# Action: all rows of ANAFULL for the same person_id have the same variables below, so one single row of PERSONS is generated
+PERSONS<-ANAFULL
+
+#create person_id as number that identifier uniquly person, used to link across tables. (Primary key)
+PERSONS<-PERSONS[,person_id:=1:.N]
+PERSONS<-copy(ANAFULL)
+
+#renamed vars
+setnames(PERSONS,"ID","person_id_src")
+setnames(PERSONS,"DATA_NASCITA","birth_date")
+setnames(PERSONS,"DATA_MORTE_MARSI","death_date")
+setnames(PERSONS,"SESSO","sex")
+
+# keep only needed vars.
+PERSONS<-PERSONS[,.(person_id,person_id_src,birth_date,sex,death_date)]
+
+fwrite(PERSONS, paste0(diroutput,"/PERSONS.csv"), quote = "auto")
+
+
+
+## OBSERVATION_PERIODS: 
+# Action: one row of ANAGRAFE_ASSISTITI generates one row of OBSERVATION_PERIODS (multiple observations per person are possible)
+
+OBSERVATION_PERIODS<-copy(ANAFULL)[,source:="ALL"][,obs_period_id:=1:.N]
+
+#usa CreateSpells prima di fare OBSERVATION_PERIODS
+
+#renamed vars
+setnames(OBSERVATION_PERIODS,"ID","person_id")
+setnames(OBSERVATION_PERIODS,"INI_RECORD","obs_period_start_date")
+setnames(OBSERVATION_PERIODS,"FINE_RECORD","obs_period_end_date")
+setnames(OBSERVATION_PERIODS,"DATA_NASCITA","birth_date")
+setnames(OBSERVATION_PERIODS,"DATA_MORTE_MARSI","death_date")
+setnames(OBSERVATION_PERIODS,"SESSO","sex")
+
+# create obs_period_end_reason
+OBSERVATION_PERIODS<-OBSERVATION_PERIODS[obs_period_end_date==date_end, obs_period_end_reason:=1]
+OBSERVATION_PERIODS<-OBSERVATION_PERIODS[obs_period_end_date==death_date, obs_period_end_reason:=2]
+OBSERVATION_PERIODS<-OBSERVATION_PERIODS[(obs_period_end_date!=death_date & obs_period_end_date!=date_end ), obs_period_end_reason:=3] #| is.na(datadec)
+
+# keep only needed vars.
+OBSERVATION_PERIODS<-OBSERVATION_PERIODS[,.(obs_period_id,person_id,source,obs_period_start_date,obs_period_end_date,obs_period_end_reason)]
+
+fwrite(OBSERVATION_PERIODS, paste0(diroutput,"/OBSERVATION_PERIODS.csv"), quote = "auto")
+
+rm(ANAFULL, PERSONS, OBSERVATION_PERIODS)
--- i_ETL_mock_data/parameters/parameters_Nordic.R
+++ i_ETL_mock_data/parameters/parameters_Nordic.R
@@ -1,10 +1,3 @@
-
-###########################################################
-# insert name of your DAP (i.e. "ARS","DepLazio","Messina")
-DAP<-"ARS"
-###########################################################
-
-
 # Call library:
 if (!require("data.table")) install.packages("data.table")
 library(data.table)
@@ -12,13 +5,17 @@ if (!require("haven")) install.packages("haven")
 library(haven)
 if (!require("lubridate")) install.packages("lubridate")
 library(lubridate)
-if (!require("stringr")) install.packages("stringr")
-library(stringr)
+
+# Define projects:
+project<-"Nordic"
 
 # Define folders:
-diroutput<-paste0(thisdir,"/20210526_BRAHMS_CDM/")
-dirinput<-paste0(thisdir,"/20210525_TheShinISS_CDM/")
-dirtemp<-paste0(thisdir,"/temp/")
+setwd("..")
+dirbase<-getwd()
+
+dirinput<-paste0(thisdir,"/i_mock_data/")
+diroutput<-paste0(dirbase,"/i_input_",project,"/")
+#dirtemp<-paste0(thisdir,"/temp/")
 
 # Check if those folders exist
 if (file.exists(diroutput)){
@@ -28,12 +25,12 @@ if (file.exists(diroutput)){
   setwd(file.path(diroutput))
 }
 
-if (file.exists(dirtemp)){
-  setwd(file.path(dirtemp))
-} else {
-  suppressWarnings(dir.create(file.path(dirtemp)))
-  setwd(file.path(dirtemp))
-}
+# if (file.exists(dirtemp)){
+#   setwd(file.path(dirtemp))
+# } else {
+#   suppressWarnings(dir.create(file.path(dirtemp)))
+#   setwd(file.path(dirtemp))
+# }
 
 setwd(thisdir)
 
@@ -46,6 +43,23 @@ end_study<-as.Date("2020-12-31")
 # lookback<-5*365.25
 # fup<-3*365.25
 
+# List all datasets:
+# spa<-paste0("SPA",rep(2007:2019))
+# num<-c()
+# for (i in rep(01:12)) {
+#   if(nchar(i)==1) {
+#     rep<-paste0(0,i)
+#     num<-c(num,rep)
+#   }
+# }
+# num<-c(num, 10:12)
+# spa_m<-paste0(spa,rep(num,each=13))
+
+# spf<-paste0("SPF",rep(2007:2019)) 
+# cap<-paste0("CAP",rep(1:2))
+# fed<-paste0("FED",rep(2007:2019))
+
+#alldatasets<-c("ID","ANA","EXE","FED","SDO", spf)
 
 ##Define study parameters
 derm_specility_sdo<-c("52") #inizia per 52
