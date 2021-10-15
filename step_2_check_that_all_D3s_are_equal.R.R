72e7cf8f83fc244fa775e41e19453ac942c50e42
RosaMGini <rosa.gini@ars.toscana.it>
2021-10-15 18:44:54 +0200

    corrected typo

--- step_2_check_that_all_D3s_are_equal.R.R
+++ step_2_check_that_all_D3s_are_equal.R.R
@@ -7,16 +7,16 @@ print(paste0("creating study variables from mock data converted to the ",CDM," C
 
 #IMPORT TABLES
 CDMcheck <- "ConcePTION"
-for (CDMcheck in c("ConcePTION","OMOP","Nordic","TheShinISS")){
+for (CDMcheck in c("ConcePTION","OMOP","Nordic","TheShinISS")) {
   temp <- fread(paste0(dirinput,"D3_",CDMcheck,".csv"))
   temp <- temp[,.(person_id,gender,age_bands,'2015','2016','2017','2018','2019')]
   for (varname in c('2015','2016','2017','2018','2019') ){
-    setnames(temp,varname",paste0(varname,CDMcheck))
+    setnames(temp,varname,paste0(varname,CDMcheck) )
   }
   if (CDM == "ConcePTION"){
     check_file <- fread(paste0(dirinput,"D3_ConcePTION.csv"))
   }else{
-    check_file <- merge(check_file,temp,all = T) 
+    check_file <- merge(check_file,temp,all = T)
   }
 }  
 
