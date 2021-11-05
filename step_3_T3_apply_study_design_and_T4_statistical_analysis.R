033a0e1946eba036f37c60b229170610719908da
Olga Paoletti <olgpao@arsanita.toscana.it>
2021-11-05 09:45:30 +0100

    correction of aggregation

--- step_3_T3_apply_study_design_and_T4_statistical_analysis.R
+++ step_3_T3_apply_study_design_and_T4_statistical_analysis.R
@@ -3,35 +3,48 @@
 D3<-fread(paste0(diroutput,"D3.csv"), skip=1, header=TRUE)
 
 agebandss<-paste0("age_bands", years)
- 
- D3_aggregate<-data.table::melt(D3,  measure = list(agebandss,years),variable.name="year",
+
+#aggregation of values by gender agebands and years
+D3_aggregated<-D3[, .(count = .N), by = c(agebandss,years,"gender")]
+
+
+ D3_melt<-data.table::melt(D3_aggregated, measure = list(agebandss,years),variable.name="year",
                                 value.name = c("age_bands","present"), na.rm = T)
 
- vect_recode_ANNI <- years
- names(vect_recode_ANNI) <- c(as.character(seq_len(length(years))))
- D3_aggregate[ , year := vect_recode_ANNI[year]]
+ vect_recode_years <- years
+ names(vect_recode_years) <- c(as.character(seq_len(length(years))))
+ D3_melt[ , year := vect_recode_years[year]]
  
-D3_aggregate<-D3_aggregate[age_bands!="",]
-D3_aggregate<-unique(D3_aggregate[,.(person_id,gender,age_bands,year)])
+ D3_melt<-D3_melt[age_bands!="",][,present:=NULL]
+
                       
-#AGGREGATION BY AGEBAND AND GENDER
-RES_ageband_by_gender<-D3_aggregate[, .(N = .N), by = c("year","age_bands","gender")]
+#sum of aggregated value
+RES_ageband_by_gender<-D3_melt[, N:=sum(count), by = c("year","age_bands","gender")]
+
+RES_ageband<-D3_melt[,.(count,year,age_bands)]
+RES_ageband<-RES_ageband[, N:=sum(count), by = c("year","age_bands")]
+
+RES_gender<-D3_melt[,.(count,year,gender)]
+RES_gender<-RES_gender[, N:=sum(count), by = c("year","gender")]
+
+#calculation of percentages by ageband and gender
+RES_ageband_by_gender<-unique(RES_ageband_by_gender[,count:=NULL])
 RES_ageband_by_gender[,perc :=round(N/sum(N), 4)*100, by = c("year","gender")]
-
-
 RES_ageband_by_gender<-RES_ageband_by_gender[order(year,age_bands,gender)]
 
 fwrite(RES_ageband_by_gender,file=paste0(diroutput,"RES_ageband_by_gender.csv"))
 
-#AGGREGATION BY AGEBAND 
-RES_ageband<-D3_aggregate[, .(N = .N), by = c("year","age_bands")]
+
+#calculation of percentages by ageband 
+RES_ageband<-unique(RES_ageband[,count:=NULL])
 RES_ageband[,perc := paste0(round(N/sum(N), 4)*100, "%"), by = c("year")]
 RES_ageband<-RES_ageband[order(year,age_bands)]
 
 fwrite(RES_ageband_by_gender,file=paste0(diroutput,"RES_ageband.csv"))
 
-#AGGREGATION BY GENDER
-RES_gender<-D3_aggregate[, .(N = .N), by = c("year","gender")]
+
+#calculation of percentages by  gender
+RES_gender<-unique(RES_gender[,count:=NULL])
 RES_gender[,perc := paste0(round(N/sum(N), 4)*100, "%"), by = c("year")]
 RES_gender<-RES_gender[order(year,gender)]
 
